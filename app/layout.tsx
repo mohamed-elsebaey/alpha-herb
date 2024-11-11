4853ab75c0197bb2918cb8f7fadbab5347dcc7ce
Mohamed <elsebaey95@outlook.com>
2024-11-11 21:39:03 +0200

    vercel/speed-insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Open_Sans, Cairo } from "next/font/google";
 
-// import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
 
 import "./globals.css";
@@ -53,7 +53,7 @@ export default async function RootLayout({
         <Template>{children}</Template>
 
         {/* website speed insights */}
-        {/* <SpeedInsights /> */}
+        <SpeedInsights />
 
         {/* website Analytics  */}
         <Analytics />
