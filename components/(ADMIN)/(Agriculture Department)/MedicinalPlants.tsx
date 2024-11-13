d5db819bfd63ecabc75dd88e7845b293dd879bd8
Mohamed <elsebaey95@outlook.com>
2024-11-13 12:32:28 +0200

    add ui folder

--- components/(ADMIN)/(Agriculture Department)/MedicinalPlants.tsx
+++ components/(ADMIN)/(Agriculture Department)/MedicinalPlants.tsx
@@ -1,3 +1,4 @@
+import InputFelid from "@/ui/InputFelid";
 import React from "react";
 
 function MedicinalPlants() {
@@ -5,196 +6,110 @@ function MedicinalPlants() {
     <div className="container mt-[120px] mb-[90px] ">
       <form className="w-[95%] md:w-[80%] mx-auto" dir="rtl">
         <div className="grid gap-6 mb-6 md:grid-cols-2 lg:grid-cols-3">
-          <InputFild
+          <InputFelid
             label="نوع المحصول"
             name="crop_type"
             type="text"
             required
             placeholder="نوع المحصول"
           />
-          <InputFild
+          <InputFelid
             label="تاريخ الزراعة"
             name="planting_date"
             type="date"
             required
             placeholder="تاريخ الزراعة"
           />
-          <InputFild
+          <InputFelid
             label="عمر النبات"
             name="plant_age"
             type="number"
             required
             placeholder="عمر النبات بالأيام"
           />
-          <InputFild
+          <InputFelid
             label="سرعة الري"
             name="watering_speed"
             type="number"
             required
             placeholder="سرعة الري "
           />
-          <div>
-            <label
-              htmlFor="watering_hours"
-              className=" mb-2 text-sm font-bold text-primary"
-            >
-              عدد ساعات الري
-            </label>
-            <input
-              type="number"
-              id="watering_hours"
-              className="bg-gray-50 border border-gray-300 text-primary text-sm rounded-lg focus:ring-primary/50 focus:border-primary/50  w-full p-2.5 "
-              placeholder="عدد ساعات الري"
-              required
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="fertilization"
-              className=" mb-2 text-sm font-bold text-primary"
-            >
-              التسميد
-            </label>
-            <input
-              type="text"
-              id="fertilization"
-              className="bg-gray-50 border border-gray-300 text-primary text-sm rounded-lg focus:ring-primary/50 focus:border-primary/50  w-full p-2.5 "
-              placeholder="التسميد"
-              required
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="generator_hours"
-              className=" mb-2 text-sm font-bold text-primary"
-            >
-              س.ت.المولد
-            </label>
-            <input
-              type="number"
-              id="generator_hours"
-              className="bg-gray-50 border border-gray-300 text-primary text-sm rounded-lg focus:ring-primary/50 focus:border-primary/50  w-full p-2.5 "
-              placeholder="س.ت.المولد"
-              required
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="breakdowns_maintenance"
-              className=" mb-2 text-sm font-bold text-primary"
-            >
-              الأعطال والصيانه
-            </label>
-            <input
-              type="text"
-              id="breakdowns_maintenance"
-              className="bg-gray-50 border border-gray-300 text-primary text-sm rounded-lg focus:ring-primary/50 focus:border-primary/50  w-full p-2.5 "
-              placeholder="الأعطال والصيانه"
-              required
-            />
-          </div>
+          <InputFelid
+            label="عدد ساعات الري"
+            name="watering_hours"
+            type="number"
+            required
+            placeholder="عدد ساعات الري"
+          />
+          <InputFelid
+            label="التسميد"
+            name="fertilization"
+            type="text"
+            required
+            placeholder="التسميد"
+          />
+          <InputFelid
+            label="س.ت.المولد"
+            name="generator_hours"
+            type="number"
+            required
+            placeholder="س.ت.المولد"
+          />
+          <InputFelid
+            label="الأعطال والصيانه"
+            name="breakdowns_maintenance"
+            type="text"
+            required
+            placeholder="الأعطال والصيانه"
+          />
         </div>
         {/* ********************************************************************* */}
         <h2 className="border-t-2 pt-4 text-2xl font-bold text-primary">
           المعدات
         </h2>
         <div className="grid gap-6 my-6  md:grid-cols-2 lg:grid-cols-3">
-          <div>
-            <label
-              htmlFor="driver_name"
-              className=" mb-2 text-sm font-bold text-primary"
-            >
-              إسم السائق
-            </label>
-            <input
-              type="text"
-              id="driver_name"
-              className="bg-gray-50 border border-gray-300 text-primary text-sm rounded-lg focus:ring-primary/50 focus:border-primary/50  w-full p-2.5 "
-              placeholder="أكتب إسم السائق"
-              required
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="equipment_type"
-              className=" mb-2 text-sm font-bold text-primary"
-            >
-              نوع المعدة
-            </label>
-            <input
-              type="text"
-              id="equipment_type"
-              className="bg-gray-50 border border-gray-300 text-primary2 text-sm rounded-lg focus:ring-primary/50 focus:border-primary/50  w-full p-2.5 "
-              placeholder="أكتب نوع المعدة"
-              required
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="working_hours"
-              className=" mb-2 text-sm font-bold text-primary"
-            >
-              ساعات العمل
-            </label>
-            <input
-              type="number"
-              id="working_hours"
-              className="bg-gray-50 border border-gray-300 text-primary text-sm rounded-lg focus:ring-primary/50 focus:border-primary/50  w-full p-2.5 "
-              placeholder="عدد ساعات العمل"
-              required
-            />
-          </div>
+          <InputFelid
+            label="إسم السائق"
+            name="driver_name"
+            type="text"
+            placeholder="إسم السائق"
+          />
+          <InputFelid
+            label="نوع المعدة"
+            name="equipment_type"
+            type="text"
+            placeholder="نوع المعدة"
+          />
+          <InputFelid
+            label="ساعات العمل"
+            name="working_hours"
+            type="number"
+            placeholder="ساعات العمل"
+          />
         </div>
         {/* ********************************************************************* */}
         <h2 className="border-t-2 pt-4 text-2xl font-bold text-primary">
           العمالة الخارجية
         </h2>
         <div className="grid gap-6 my-6  md:grid-cols-2 lg:grid-cols-3">
-          <div>
-            <label
-              htmlFor="contractor"
-              className=" mb-2 text-sm font-bold text-primary"
-            >
-              إسم المقاول
-            </label>
-            <input
-              type="text"
-              id="contractor"
-              className="bg-gray-50 border border-gray-300 text-primary text-sm rounded-lg focus:ring-primary/50 focus:border-primary/50  w-full p-2.5 "
-              placeholder="إسم المقاول"
-              required
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="num_workers"
-              className=" mb-2 text-sm font-bold text-primary"
-            >
-              عدد العمال
-            </label>
-            <input
-              type="num"
-              id="num_workers"
-              className="bg-gray-50 border border-gray-300 text-primary2 text-sm rounded-lg focus:ring-primary/50 focus:border-primary/50  w-full p-2.5 "
-              placeholder="أكتب عدد العمال"
-              required
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="working_hours2"
-              className=" mb-2 text-sm font-bold text-primary"
-            >
-              ساعات العمل
-            </label>
-            <input
-              type="number"
-              id="working_hours2"
-              className="bg-gray-50 border border-gray-300 text-primary text-sm rounded-lg focus:ring-primary/50 focus:border-primary/50  w-full p-2.5 "
-              placeholder="عدد ساعات العمل"
-              required
-            />
-          </div>
+          <InputFelid
+            label="إسم المقاول"
+            name="contractor"
+            type="text"
+            placeholder="إسم المقاول"
+          />
+          <InputFelid
+            label="عدد العمال"
+            name="num_workers"
+            type="number"
+            placeholder="عدد العمال"
+          />
+          <InputFelid
+            label="ساعات العمل"
+            name="working_hours2"
+            type="number"
+            placeholder="عدد ساعات العمل"
+          />
         </div>
 
         <div className="my-12 text-center">
@@ -212,32 +127,3 @@ function MedicinalPlants() {
 
 export default MedicinalPlants;
 
-const InputFild = ({
-  label,
-  name,
-  type,
-  placeholder,
-  required,
-}: {
-  label: string;
-  name: string;
-  type: string;
-  placeholder: string;
-  required?: boolean;
-}) => {
-  return (
-    <div>
-      <label htmlFor={name} className=" mb-2 text-sm font-bold text-primary">
-        {label}
-      </label>
-      <input
-        id={name}
-        name={name}
-        type={type}
-        className="mt-2 bg-gray-50 border border-gray-300 text-primary text-sm rounded-lg focus:ring-primary/50 focus:border-primary/50  w-full p-2.5 focus:outline-none focus:ring-1"
-        placeholder={placeholder}
-        required={required}
-      />
-    </div>
-  );
-};
---
+++ ui/InputFelid.tsx
@@ -0,0 +1,31 @@
+const InputFelid = ({
+  label,
+  name,
+  type,
+  placeholder,
+  required,
+}: {
+  label: string;
+  name: string;
+  type: string;
+  placeholder: string;
+  required?: boolean;
+}) => {
+  return (
+    <div>
+      <label htmlFor={name} className=" mb-2 text-sm font-bold text-primary">
+        {label}
+      </label>
+      <input
+        id={name}
+        name={name}
+        type={type}
+        className="mt-2 bg-gray-50 border border-gray-300 text-primary text-sm rounded-lg focus:ring-primary/50 focus:border-primary/50  w-full p-2.5 focus:outline-none focus:ring-1"
+        placeholder={placeholder}
+        required={required}
+      />
+    </div>
+  );
+};
+
+export default InputFelid;
