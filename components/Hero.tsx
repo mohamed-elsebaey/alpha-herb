4e8f9c6d4247c4082ce186cc945cc51ba9e67148
Mohamed <elsebaey95@outlook.com>
2025-04-24 01:23:32 +0200

    in hero component change hero image with 3 Random videos

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,12 @@
-import Image from "next/image";
 import Link from "next/link";
 
-import heroImage from "@/public/hero.png";
+const videosNames = [
+  "1",
+  "2",
+  "3",
+];
+
+const randomVideo = videosNames[Math.floor(Math.random() * 3)];
 
 function Hero() {
   return (
@@ -26,50 +31,19 @@ function Hero() {
           </Link>
         </div>
         <div className="w-full lg:w-6/12 flex justify-center">
-          <div className="relative z-10 inline-block pt-11 lg:pt-0">
-            <Image
-              width={700}
-              height={700}
-              src={heroImage}
-              alt="hero"
-              className="max-w-full lg:ml-auto rounded-lg rounded-tl-[100px]"
-              priority
-            />
-            <span className="absolute -bottom-8 -left-8 z-[-1] hidden lg:block">
-              <svg
-                width="93"
-                height="93"
-                viewBox="0 0 93 93"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <circle cx="2.5" cy="2.5" r="2.5" fill="#115934" />
-                <circle cx="2.5" cy="24.5" r="2.5" fill="#115934" />
-                <circle cx="2.5" cy="46.5" r="2.5" fill="#115934" />
-                <circle cx="2.5" cy="68.5" r="2.5" fill="#115934" />
-                <circle cx="2.5" cy="90.5" r="2.5" fill="#115934" />
-                <circle cx="24.5" cy="2.5" r="2.5" fill="#115934" />
-                <circle cx="24.5" cy="24.5" r="2.5" fill="#115934" />
-                <circle cx="24.5" cy="46.5" r="2.5" fill="#115934" />
-                <circle cx="24.5" cy="68.5" r="2.5" fill="#115934" />
-                <circle cx="24.5" cy="90.5" r="2.5" fill="#115934" />
-                <circle cx="46.5" cy="2.5" r="2.5" fill="#115934" />
-                <circle cx="46.5" cy="24.5" r="2.5" fill="#115934" />
-                <circle cx="46.5" cy="46.5" r="2.5" fill="#115934" />
-                <circle cx="46.5" cy="68.5" r="2.5" fill="#115934" />
-                <circle cx="46.5" cy="90.5" r="2.5" fill="#115934" />
-                <circle cx="68.5" cy="2.5" r="2.5" fill="#115934" />
-                <circle cx="68.5" cy="24.5" r="2.5" fill="#115934" />
-                <circle cx="68.5" cy="46.5" r="2.5" fill="#115934" />
-                <circle cx="68.5" cy="68.5" r="2.5" fill="#115934" />
-                <circle cx="68.5" cy="90.5" r="2.5" fill="#115934" />
-                <circle cx="90.5" cy="2.5" r="2.5" fill="#115934" />
-                <circle cx="90.5" cy="24.5" r="2.5" fill="#115934" />
-                <circle cx="90.5" cy="46.5" r="2.5" fill="#115934" />
-                <circle cx="90.5" cy="68.5" r="2.5" fill="#115934" />
-                <circle cx="90.5" cy="90.5" r="2.5" fill="#115934" />
-              </svg>
-            </span>
+          <div className="relative z-10 inline-block pt-11 lg:pt-0 w-full bg-white shadow-lg rounded-lg rounded-tl-[80px] overflow-hidden">
+            <video
+              src={`https://res.cloudinary.com/dyryptpqq/video/upload/v1/Alphaherb-videos/${randomVideo}`}
+              className="w-full aspect-video"
+              autoPlay
+              loop
+              muted
+              playsInline
+              controls={false}
+              title="Hero Video"
+            >
+              Your browser does not support the video tag.
+            </video>
           </div>
         </div>
       </div>
